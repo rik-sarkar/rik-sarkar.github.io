eb5f449660e75b47e9c96ba9f06e2142b67cbbb6
rik-sarkar <ps990@scarletmail.rutgers.edu>
2020-05-02 05:19:47 -0400

    another change

---
+++ Processing Files/planets.js
@@ -0,0 +1,18 @@
+void setup () {
+  size (900, 900);
+  background(14,7,42);
+  noStroke();
+  smooth();
+  fill(0);
+  ellipse(56, 46, 50, 50);
+  fill(100);
+  ellipse(156, 146, 100, 100);
+  fill(0);
+  ellipse(256, 246, 150, 150);
+  fill(100);
+  ellipse(356, 346, 200, 200);
+  fill(0);
+  ellipse(556, 546, 500, 500);
+  fill(100);
+  ellipse(756, 746, 400, 400);
+}
